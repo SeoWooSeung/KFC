6f38d1196d2773937798f34ab55c1712465bbc1c
서우승 <148739245+SeoWooSeung@users.noreply.github.com>
2023-11-24 15:01:51 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,8 @@ $(function () {
     $('.visual_slide').slick({
         arrows: false,
         autoplay: true,
-        pauseOnHover: false,
-        // dots: true,
+        // pauseOnHover: false,
+        dots: true,
     });
 
     $('.main_visual .arrows .left').on('click', function () {
@@ -21,6 +21,38 @@ $(function () {
         e.preventDefault();
         const idx = $(this).index();
         $('.visual_slide').slick('slickGoTo', idx);
+    });
+
+
+
+    $('.main_product_slide').slick({
+        arrows: false,
+        autoplay: true,
+        // pauseOnHover: false,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+
+    $('.slide_Content .arrows .left').on('click', function () {
+        //슬라이드가 뒤로 가 : slick 홈페이지 참조
+        $('.main_product_slide').slick('slickPrev')
+    });
+
+    $('.slide_Content .arrows .right').on('click', function () {
+        //슬라이드가 뒤로 가 : slick 홈페이지 참조
+        $('.main_product_slide').slick('slickNext')
+    });
+
+
+    $('.slide_Content').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index();
+        $('.main_product_slide').slick('slickGoTo', idx);
+    });
+
+    $('.totop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 800)
     });
 
 });
